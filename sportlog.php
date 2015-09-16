8b5074a1dc7ac82c1bb995dc4bee6571ca6b68e2
Michael Meer <michael@meer.name>
2015-09-16 11:02:42 +0200

    try to understand to hook concept

--- sportlog.php
+++ sportlog.php
@@ -10,17 +10,21 @@
 
 function sportlog_load() {
     register_hook('app_menu', 'addon/sportlog/sportlog.php', 'sportlog_app_menu');
+    register_hook('post_local', 'addon/sportlog/sportlog.php', 'sportlog_post_hook');
+    logger('SportLog Addon initiated');
 }
 
 function sportlog_unload() {
     unregister_hook('app_menu', 'addon/sportlog/sportlog.php', 'sportlog_app_menu');
-
+    unregister_hook('post_local', 'addon/sportlog/sportlog.php', 'sportlog_post_hook');
+    logger('SportLog Addon unloaded');
 }
 
 function sportlog_app_menu($a,&$b) {
     $b['app_menu'][] = '<div class="app-title"><a href="sportlog">Sportlog</a></div>';
 }
 
+function sportlog_post_hook() {}
 
 function sportlog_module() {}
 
