ed251b74dd614c5e426300850cfad8435685a760
Michael Meer <michael@meer.name>
2015-09-16 11:31:58 +0200

    some classes for the data

---
+++ personaldata.php
@@ -0,0 +1,17 @@
+<?php
+class PersonalData
+{
+	private $who;
+	private $timestamp;
+	private $weight;
+	private $weightunit;
+	private $age;
+	private $gender;
+	private $hrmax;
+
+	public function store() {}
+	public function get() {}	
+	public function edit() {}
+	public function del() {}
+}
+?>
---
+++ training.php
@@ -0,0 +1,21 @@
+<?php
+class Training
+{
+	private $who;
+	private $timestamp;
+	private $duration;
+	private $kindofsport;
+	private $distance;
+	private $distanceunit;
+	private $heightunit;
+	private $burnedkcals;
+	private $avheartrate;
+	private $avsteps;
+	private $equipment;
+
+	public function store() {}
+	public function get() {}	
+	public function edit() {}
+	public function del() {}
+}
+?>
