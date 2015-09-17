f18efea9925d8d5ce5c5a75d4624ab456604b3b4
Michael Meer <michael@meer.name>
2015-09-17 15:31:02 +0200

    typo

--- sportlog.php
+++ sportlog.php
@@ -1,50 +1,42 @@
 <?php
 
 /**
- * Name: Sportlog
- * Description: to track your sportive activities
+ * Name: Sport Log
+ * Description: Track your sportive activities
  * Version: 1.0
  * Author: Michael Meer
  */
 
-require addon/personaldata.php;
-require addon/training.php;
 
 function sportlog_load() {
-    register_hook('app_menu', 'addon/sportlog/sportlog.php', 'sportlog_app_menu');
-    register_hook('post_local', 'addon/sportlog/sportlog.php', 'sportlog_post_hook');
-    logger('SportLog Addon initiated');
+  register_hook('app_menu', 'addon/sportlog/sportlog.php', 'sportlog_app_menu');
 }
 
 function sportlog_unload() {
     unregister_hook('app_menu', 'addon/sportlog/sportlog.php', 'sportlog_app_menu');
-    unregister_hook('post_local', 'addon/sportlog/sportlog.php', 'sportlog_post_hook');
-    logger('SportLog Addon unloaded');
+
 }
 
 function sportlog_app_menu($a,&$b) {
-	logger(inside sportlog_app_menu);
-    $b['app_menu'][] = '<div class="app-title"><a href="sportlog">Sportlog</a></div>';
+    $b['app_menu'][] = '<div class="app-title"><a href="sportlog">Mahjongg</a></div>';
 }
 
-function sportlog_install() {
-	logger(inside sportlog_install);
-}
 
-function sportlog_uninstall() {
-	logger(inside sportlog_uninstall);
-}
-
-function sportlog_post_hook() {
-	logger(inside sportlog_post_hook);
-}
-
-function sportlog_module() {
-	logger(inside sportlog_module);
-}
+function sportlog_module() {}
 
 function sportlog_content(&$a) {
 
 $baseurl = $a->get_baseurl() . '/addon/sportlog';
 
+$o .= <<< EOT
+<br><br>
+<p align="left">
+<embed src="addon/sportlog/sportlog.swf" quality="high" bgcolor="#FFFFFF" width="800" height="600" name="sportlog" align="middle" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" />
+<br><br>
+<b>Simply locate the matching tiles and find a way to clear them from the board as quickly as possible.
+A timer at the top of the screen keeps track of how you are doing.</b><br>
+</p>
+EOT;
+
+return $o;
 }
